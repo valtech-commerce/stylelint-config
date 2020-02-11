10f81d1100f7363462d33e29aad9c6bf47e482b3
Jean-Baptiste Landry <jb.landry@valtech.com>
2020-02-11 11:00:12 -0500

    Update manager/tester

---
+++ test/feature/config-tests.js
@@ -0,0 +1,30 @@
+//--------------------------------------------------------
+//-- Config tests
+//--------------------------------------------------------
+export default ({ given, when, then }) => {
+
+	beforeEach(() => {
+		given.noException();
+		given.noRoot();
+		given.noConfig();
+		given.noResults();
+		given.currentRoot();
+	});
+
+
+	test(`Ensure YAML is parsable`, () => {
+		when.configIsLoaded();
+		then.shouldNotHaveThrown();
+	});
+
+	test(`Ensure file contains config`, () => {
+		when.configIsLoaded();
+		then.configShouldNotBeEmpty();
+	});
+
+	test(`Ensure config can be used`, async () => {
+		await when.configIsUsed();
+		then.resultsShouldContainNoError();
+	});
+
+};
