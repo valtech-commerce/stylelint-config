a3d122b26beae8653e26e88050fdb7c688e1fbd3
Jean-Baptiste Landry <jb.landry@valtech.com>
2023-03-14 15:43:50 -0400

    Remove pkg-dir

--- src/get-config.js
+++ src/get-config.js
@@ -4,15 +4,12 @@
 const path = require("node:path");
 const { fsSync } = require("@valtech-commerce/fs");
 const deepMerge = require("deepmerge");
-const pkgDir = require("pkg-dir");
 
 module.exports = (...paths) => {
 	return paths.reduce((config, filePath) => {
 		const filePaths = filePath.split("/");
 		const filename = filePaths.pop();
-		const current = fsSync.readYaml(
-			path.join(pkgDir.sync(__dirname), "extendable-config", ...filePaths, `${filename}.yaml`)
-		);
+		const current = fsSync.readYaml(path.join(__dirname, "..", "extendable-config", ...filePaths, `${filename}.yaml`));
 
 		return deepMerge.all([config, current], {
 			arrayMerge: (destination, source) => source,
